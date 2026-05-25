f478afc1fdbafeccd591209fef1ee9ecb53d40d6
Elad0109 <86915722+Elad0109@users.noreply.github.com>
2026-05-25 10:48:23 +0300

    Add files via upload

---
+++ test/test_app.py
@@ -0,0 +1,21 @@
+from src.app import add_two_numbers
+
+
+def test_add_positive_integers():
+    assert add_two_numbers(2, 3) == 5
+
+
+def test_add_negative_integers():
+    assert add_two_numbers(-4, -6) == -10
+
+
+def test_add_mixed_sign_integers():
+    assert add_two_numbers(-4, 10) == 6
+
+
+def test_add_with_zero():
+    assert add_two_numbers(0, 7) == 7
+
+
+def test_add_floats():
+    assert add_two_numbers(2.5, 3.1) == 5.6
